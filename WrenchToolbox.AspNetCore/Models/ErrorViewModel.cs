0402f45937edc1567d2ef5055bf38255128921f6
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-07-27 12:32:55 +0430

    Add project files.

---
+++ WrenchToolbox.AspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WrenchToolbox.AspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
